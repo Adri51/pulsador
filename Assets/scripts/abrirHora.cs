2d25ec78d1bcb3ac9ea02d2df8429f22c84f5b2a
adri51 <adriibm91@gmail.com>
2020-02-22 19:12:24 +0100

    assets y unity

---
+++ Assets/scripts/abrirHora.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abrirHora : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void abrirPanel(){
+        if(Panel !=null){
+            Panel.SetActive(true);
+        }
+    }
+}
